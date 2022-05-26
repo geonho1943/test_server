7f38c945077bae18fe001a2553e0cb0277528345
a <a@DESKTOP-E8MAQGC>
2022-05-27 02:48:52 +0900

    new_test

---
+++ test/src/main/java/com/my/test/dao/UserDao.java
@@ -0,0 +1,33 @@
+package com.my.test.dao;
+
+import java.util.List;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+import com.my.test.vo.User;
+
+public class UserDao extends SqlSessionDaoSupport{
+	public List<User> getAllUser() {
+		return this.getSqlSession().selectList("ns_user.getAllUser");
+	}
+	
+	public User getOneUser(int user_idx) {
+		return this.getSqlSession().selectOne("ns_user.getOneUser",user_idx);
+	}
+	
+	public int insertUser(User user) {
+		return this.getSqlSession().insert("ns_user.insertUser", user);
+	}
+	
+	public int updateUser(User user) {
+		return this.getSqlSession().update("ns_user.updateUser",user);
+	}
+
+ 	public int deleteUser(int user_idx) {
+		return this.getSqlSession().delete("ns_user.deleteUser",user_idx);
+	}
+ 	
+ 	public User getUserByIdAndPw(User user) {
+		return this.getSqlSession().selectOne("ns_user.getUserByIdAndPw",user);
+	}
+}
---
+++ test/src/main/java/com/my/test/vo/Comm.java
@@ -0,0 +1,39 @@
+package com.my.test.vo;
+
+public class Comm{
+	int com_idx = 0;
+	int com_doc_idx = 0;
+	String com_writ =null;
+	String com_cont =null;
+	String com_reg = null;
+	public int getCom_idx() {
+		return com_idx;
+	}
+	public void setCom_idx(int com_idx) {
+		this.com_idx = com_idx;
+	}
+	public int getCom_doc_idx() {
+		return com_doc_idx;
+	}
+	public void setCom_doc_idx(int com_doc_idx) {
+		this.com_doc_idx = com_doc_idx;
+	}
+	public String getCom_writ() {
+		return com_writ;
+	}
+	public void setCom_writ(String com_writ) {
+		this.com_writ = com_writ;
+	}
+	public String getCom_cont() {
+		return com_cont;
+	}
+	public void setCom_cont(String com_cont) {
+		this.com_cont = com_cont;
+	}
+	public String getCom_reg() {
+		return com_reg;
+	}
+	public void setCom_reg(String com_reg) {
+		this.com_reg = com_reg;
+	}
+}
